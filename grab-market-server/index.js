1cc7d2e307198f383190b830e1686301508cddd9
dldntjr0516 <dldntjr0516@gmail.com>
2022-11-16 19:29:42 +0900

    heroku 배포 준비

--- grab-market-server/index.js
+++ grab-market-server/index.js
@@ -1,34 +1,156 @@
-var http = require("http"); // 내장 모듈 (http)
-var hostname = "127.0.0.1"; // 내부 아이피 (내 컴퓨터 주소)
-var port = 8080;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const models = require("./models");
+const multer = require("multer");
+const port = process.env.PORT || 8080;
 
-const server = http.createServer(function (req, res) {
-  const path = req.url;
-  const method = req.method;
+// json 형식을 사용하겠음을 의미.
+app.use(express.json());
+// cors를 통해 모든 브라우저가 내 서버에 요청할 수 있게 됨.
+app.use(cors());
+// 서버에서 이미지 파일들을 보여줄 때, 우리가 입력했던 경로로 보여주기 위해 설정한 것.
+app.use("/uploads", express.static("uploads"));
 
-  // 경로가 '/products'인 경우,
-  if (path === "/products") {
-    // 요청 방식이 'GET'인 경우,
-    if (method === "GET") {
-      // 응답 Header에 status code를 200, json 형태로 반환.
-      res.writeHead(200, { "Content-Type": "applicaton/json" }); // status code, json 형태의 응답
-      const products = JSON.stringify([
-        {
-          name: "농구공",
-          price: 5000,
-        },
-      ]);
-      // products를 응답으로 반환.
-      res.end(products);
-    } else if (method === "POST") {
-      res.end("생성되었습니다!");
-    }
-  } else {
-    res.end("Good Bye");
-  }
+// multer 파일 저장 경로 설정
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
 });
 
-// 요청을 기다리고 있음을 의미.
-server.listen(port, hostname);
+app.get("/banners", (req, res) => {
+  models.Banner.findAll({
+    limit: 2,
+  })
+    .then((result) => {
+      res.send({
+        banners: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("에러가 발생했습니다.");
+    });
+});
 
-console.log("grab market server on!");
+// '/products'라는 경로에 'GET'으로 요청이 온 경우 콜백 함수가 실행됨.
+app.get("/products", (req, res) => {
+  models.Product.findAll({
+    order: [["createdAt", "DESC"]],
+    attributes: [
+      "id",
+      "name",
+      "price",
+      "createdAt",
+      "seller",
+      "imageUrl",
+      "soldout",
+    ],
+  })
+    .then((result) => {
+      console.log("PRODUCTS : ", result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("에러 발생");
+    });
+});
+
+// '/products'라는 경로에 'POST'로 요청이 온 경우 콜백 함수가 실행됨.
+app.post("/products", (req, res) => {
+  const body = req.body;
+  const { name, description, price, seller, imageUrl } = body;
+
+  if (!name || !description || !price || !seller || !imageUrl) {
+    res.status(400).send("모든 필드를 입력해주세요.");
+  }
+
+  models.Product.create({ name, description, price, seller, imageUrl })
+    .then((result) => {
+      console.log("상품 생성 결과 : ", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("상품 업로드에 문제가 발생했습니다.");
+    });
+});
+
+// id를 통해 상품을 조회.
+app.get("/products/:id", (req, res) => {
+  const params = req.params;
+  const { id } = params;
+  models.Product.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then((result) => {
+      console.log("PRODUCT : ", result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).send("상품 조회에 에러가 발생했습니다.");
+    });
+});
+
+app.post("/image", upload.single("image"), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  res.send({
+    imageUrl: file.path,
+  });
+});
+
+app.post("/purchase/:id", (req, res) => {
+  const { id } = req.params;
+
+  models.Product.update(
+    {
+      soldout: 1,
+    },
+    {
+      where: {
+        id,
+      },
+    }
+  )
+    .then((result) => {
+      res.send({
+        result: true,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("에러가 발생했습니다.");
+    });
+});
+
+// 요청 대기 상태
+app.listen(port, () => {
+  console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다.");
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("DB 연결 성공!");
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log("DB 연결 에러ㅠ");
+      process.exit();
+    });
+});
